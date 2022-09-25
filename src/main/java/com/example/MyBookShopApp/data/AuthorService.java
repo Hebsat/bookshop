486d2862181bf1a225a7662d6bd96d4e3553a136
Yuri Kaleganov <yurkaleganov@yandex.ru>
2022-09-25 17:55:18 +0300

    init

---
+++ src/main/java/com/example/MyBookShopApp/data/AuthorService.java
@@ -0,0 +1,31 @@
+package com.example.MyBookShopApp.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class AuthorService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public AuthorService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Map<String, List<Author>> getAuthorData() {
+        List<Author> authors = jdbcTemplate.query("SELECT * FROM authors", (ResultSet rs, int rowNum) -> {
+                    Author author = new Author();
+                    author.setId(rs.getInt("id"));
+                    author.setName(rs.getString("author_name"));
+                    return author;
+                });
+        return authors.stream().collect(Collectors.groupingBy(a -> a.getName().substring(0, 1)));
+    }
+}
